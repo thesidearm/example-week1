cfa963ffbbec2ebcf365c724475d6e92c1164a9e
thesidearm <b00705033@ntu.edu.tw>
2014-05-02 16:45:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 /**
- * @overview
- *
- * @author
- * @version 2014/04/26
+ * @JSXIM hw1
+ *  Js練習
+ * @B00705033 陳秋中
+ * @version 2014/04/27
  */
 
 var http = require("http");
-var port = 1337;
+//開http用變數 並載入模組
 var request = require("request");
-var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
+//開request用變數 並載入模組
+var url = "http://graph.facebook.com/GameOfThrones/photos?type=uploaded";
+var port = 1337;
 
-http.createServer(function (req, res) {
-  res.writeHeader(200, {"Content-Type": "text/html"});
-  
-  var data = "<html><head><title>hello world</title></head><body>"
-  request.get(url, function (err, body, response) {
+http.createServer(function (req, res){
+ 
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    
+    var data ="<html><head><style>table,th,td{border:5px solid black}</style></head><body><table style='width:850px'>";
+    //style中建立表格
+    
+    request.get(url, function(err, body, result){
+    result = JSON.parse(result);    //因為JSON是global所以不需載入
+	  result.data.forEach(function(val, idx){	
+	 	
+        data += "<tr><td>><img src='" + val.images[2].source + "'width='600'/></td>";
+	      data += "<td><Font color='181789'size='6'face='微軟正黑體'><p>" + val.name + "</p></Font></td></tr>";
+    });
 
-    response = JSON.parse(response);
-    response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'>";
+    data += "</body><html>";
+    res.end(data); //回應在網頁端
+    
     });
     
-    data += "</body></html>";
-    res.end(data);
-  });
-
 }).listen(port);
 
-console.log("start server port: " + port);
-
-
+console.log("server create: " + port);
